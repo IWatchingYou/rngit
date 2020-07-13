4fc292415e02f0eaa751c0d64299e37b078cf498
sokphaladam <sokphaladam@gmail.com>
2020-07-13 15:47:15 +0700

    Updates

---
+++ precache-manifest.bb97f3c165393cbcb61d461da2339d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7853a3e0f75018f4a5ae78bd51883c",
+    "url": "/rngit/index.html"
+  },
+  {
+    "revision": "dbe042eee95ddfd04080",
+    "url": "/rngit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b90db1e860a9ddf97f9",
+    "url": "/rngit/static/js/2.b50926c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rngit/static/js/2.b50926c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe042eee95ddfd04080",
+    "url": "/rngit/static/js/main.90a68878.chunk.js"
+  },
+  {
+    "revision": "795eea29a1f34f90ab22",
+    "url": "/rngit/static/js/runtime-main.a23e4fe6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rngit/static/media/logo.5d5d9eef.svg"
+  }
+]);
